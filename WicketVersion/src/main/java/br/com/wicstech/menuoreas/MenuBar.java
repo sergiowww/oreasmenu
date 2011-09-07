ba63604db5566c21caca416e028c52c4f5f85366
sergiowww@gmail.com <sergiowww@gmail.com@dec59da4-91f7-11de-9897-6f94e350a523>
2011-09-07 04:06:41 +0000

    Correções e melhorias

--- WicketVersion/src/main/java/br/com/wicstech/menuoreas/MenuBar.java
+++ WicketVersion/src/main/java/br/com/wicstech/menuoreas/MenuBar.java
@@ -1,19 +1,36 @@
 package br.com.wicstech.menuoreas;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.wicket.behavior.HeaderContributor;
+import org.apache.commons.lang.StringUtils;
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.IHeaderContributor;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.model.util.ListModel;
 
 /**
  * Barra de menu.
  * @author Sergio
  *
  */
-public class MenuBar extends HeaderContributor {
+public class MenuBar extends WebMarkupContainer implements IHeaderContributor{
 
 	private static final long serialVersionUID = -8683400812756686255L;
-	private MenuBarBuilder menuBarBuilder;
+	private static final String CONFIG_PART_INSERT_ELEMENT = "#{parentElement}",
+	CONFIG_PART_INSERT_POS = "#{insertion}";
+
+	
+	private static final ResourceReference JS_PROTOTYPE = new ResourceReference(MenuBar.class, "libraries/prototype.js"),
+	JS_OREASMENU_CONFIG = new ResourceReference(MenuBar.class, "js/oreasmenuConfig.js"),
+	JS_OREASMENU = new ResourceReference(MenuBar.class, "js/oreasmenu.js");
+	
+	private String insertPosition = "top";
+	private String idContainer;
+	private List<Nivel> niveis = new ArrayList<Nivel>();
+	private Integer idMenuItem = 0;
 	
 	/**
 	 * 
@@ -21,19 +38,150 @@ public class MenuBar extends HeaderContributor {
 	 * @param menuItens
 	 */
 	public MenuBar(String id, List<MenuItem> menuItens) {
-		this(new MenuBarBuilder(id, menuItens));
-	}
-	private MenuBar(IHeaderContributor headerContributor) {
-		super(headerContributor);
-		this.menuBarBuilder = (MenuBarBuilder) headerContributor;
+		super(id, new ListModel<MenuItem>(menuItens));
+		setOutputMarkupId(true);
+		this.idContainer = getMarkupId();
 	}
 	
+
 	/**
 	 * Adicionar um nível no menu.
 	 * @param nivel
 	 */
 	public void addNivel(Nivel nivel) {
-		menuBarBuilder.addNivel(nivel);
+		niveis.add(nivel);
 	}
 
+
+	public void renderHead(IHeaderResponse response) {
+		response.renderJavascriptReference(JS_PROTOTYPE, "prototype");
+		response.renderJavascriptReference(JS_OREASMENU, "oreasmenu");
+		renderConfig(response);
+		renderMenu(response);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void renderMenu(IHeaderResponse response) {
+		StringBuilder javascript = new StringBuilder("Event.observe(window, \"load\", function(){");
+		javascript.append("var menus = new Array();\r\n");
+		List<MenuItem> menuItens = (List<MenuItem>) getDefaultModelObject();
+		for (MenuItem menuItem : menuItens) {
+			buildJsMenu(menuItem, javascript, null);
+		}
+		
+		javascript.append("FactoryMenu.construirMenu(menus);");
+		javascript.append("});");
+		response.renderJavascript(javascript, "buildMenu");
+	}
+	
+	private String getStringParam(CharSequence valor){
+		if(valor == null){
+			return ""+valor;
+		}
+		return "\""+valor+"\"";
+	}
+
+	
+	private void buildJsMenu(MenuItem menuItem, StringBuilder javascript,
+			String parentMenu) {
+		
+
+        String menuName = "menu"+(idMenuItem++);
+        javascript.append("var ");
+        javascript.append(menuName);
+        javascript.append(" = new MenuItem(");
+		javascript.append(getStringParam(menuItem.getTitulo()));
+		javascript.append(',');
+		javascript.append(getStringParam(getImageUrl(menuItem.getImagem())));
+        javascript.append(',');
+        javascript.append(""+menuItem.getWidth());
+        javascript.append(',');
+        javascript.append(""+menuItem.getHeight());
+        javascript.append(',');
+        javascript.append(getStringParam(menuItem.getPage()));
+        javascript.append(',');
+        javascript.append(getStringParam(menuName));
+        javascript.append(',');
+        javascript.append(getStringParam(menuItem.getTarget()));
+        javascript.append(',');
+        javascript.append(""+menuItem.getOnclick());
+        javascript.append(',');
+        javascript.append(""+menuItem.getOnmouseover());
+        javascript.append(',');
+        javascript.append(""+menuItem.getOnmouseout());
+        javascript.append(',');
+        javascript.append(getStringParam(menuItem.getAlign()));
+        javascript.append(");\r\n");
+        if(parentMenu != null){
+        	javascript.append(menuName);
+        	javascript.append(".setParentMenuItem(");
+        	javascript.append(parentMenu);
+        	javascript.append(");\r\n");
+        }else{
+        	javascript.append("menus.push(");
+        	javascript.append(menuName);
+        	javascript.append(");\r\n");
+        }
+		for (MenuItem subMenuItem : menuItem.getSubMenus()) {
+			buildJsMenu(subMenuItem, javascript, menuName);
+		}
+	}
+
+
+	private CharSequence getImageUrl(ResourceReference imagem) {
+		if(imagem != null){
+			return RequestCycle.get().urlFor(imagem);
+		}
+		return null;
+	}
+
+
+	private void renderConfig(IHeaderResponse response) {
+		String config = MenuUtil.getResourceContentAsString(JS_OREASMENU_CONFIG);
+		config = StringUtils.replace(config, CONFIG_PART_INSERT_ELEMENT, idContainer);
+		config = StringUtils.replace(config, CONFIG_PART_INSERT_POS, insertPosition);
+		StringBuilder builderConfig = new StringBuilder(config);
+		for (Nivel nivel : this.niveis) {
+			builderConfig.append("FactoryMenu.addNivel(new Nivel(");
+			builderConfig.append(nivel.isOrientacao());
+			
+			builderConfig.append(",");
+			builderConfig.append(getStringParam(nivel.getStyleClass()));
+			builderConfig.append("");
+			
+			builderConfig.append(",");
+			builderConfig.append(getStringParam(nivel.getStyleClassHover()));
+			builderConfig.append("");
+			
+			if(nivel.getImagemSeta() == null){
+				builderConfig.append(", null");
+			}else{
+				builderConfig.append(",");
+				builderConfig.append(getStringParam(getImageUrl(nivel.getImagemSeta())));
+				builderConfig.append("");
+			}
+			builderConfig.append(',');
+			builderConfig.append(nivel.getWidthDefault());
+			builderConfig.append(',');
+			builderConfig.append(nivel.getHeightDefault());
+			builderConfig.append("));\r\n");
+		}
+		response.renderJavascript(builderConfig, "menuBuildConfig");
+	}
+
+
+	/**
+	 * @return the insertPosition
+	 */
+	public String getInsertPosition() {
+		return insertPosition;
+	}
+
+
+	/**
+	 * @param insertPosition the insertPosition to set
+	 */
+	public void setInsertPosition(String position) {
+		this.insertPosition = position;
+	}
 }
--- WicketVersion/src/main/java/br/com/wicstech/menuoreas/MenuItem.java
+++ WicketVersion/src/main/java/br/com/wicstech/menuoreas/MenuItem.java
@@ -9,87 +9,111 @@ import org.apache.wicket.ResourceReference;
 
 /**
  * Item de menu.
+ * 
  * @author Sergio
- *
+ * 
  */
-public class MenuItem implements Serializable{
+public class MenuItem implements Serializable {
 	private static final long serialVersionUID = 4602042305416858668L;
 
 	/**
 	 * Título do menu.
 	 */
 	private String titulo,
-	
+
 	/**
 	 * Página de destino do link.
 	 */
 	page,
-	
+
 	/**
 	 * Janela destino do menu (_blank, _self)
 	 */
 	target,
-	
+
 	/**
 	 * Alinhamento do texto do menu.
 	 */
 	align,
-	
+
 	/**
 	 * Dica da ferramenta
 	 */
 	toolTip,
-	
+
 	/**
 	 * Evento javascript ao clicar no item de menu.
 	 */
 	onclick,
-	
+
 	/**
 	 * Evento javascript ao passar o mouse por cima do item de menu.
 	 */
 	onmouseover,
-	
+
 	/**
 	 * Ao retirar o mouse de cima do item de menu.
 	 */
 	onmouseout;
-	
+
 	/**
 	 * Imagem do menu.
 	 */
 	private ResourceReference imagem;
-	
+
 	/**
 	 * Largura do item de menu.
 	 */
 	private Integer width,
-	
+
 	/**
 	 * Altura do item de menu.
 	 */
 	height;
-	
+
 	/**
 	 * Lista de submenus.
 	 */
 	private List<MenuItem> subMenus = new ArrayList<MenuItem>();
-	
-	
 
-	public MenuItem(String titulo, ResourceReference imagem, CharSequence urlDestino) {
+	/**
+	 * Construtor do menu com um título, imagem e url de destino do menu.
+	 * 
+	 * @param titulo
+	 * @param imagem
+	 * @param urlDestino
+	 */
+	public MenuItem(String titulo, ResourceReference imagem,
+			CharSequence urlDestino) {
 		this.titulo = titulo;
 		this.imagem = imagem;
-		if(urlDestino != null){
+		if (urlDestino != null) {
 			this.page = urlDestino.toString();
 		}
-		
 	}
+
+	/**
+	 * Construtor do menu somente com título.
+	 * 
+	 * @param titulo
+	 */
+	public MenuItem(String titulo) {
+		this.titulo = titulo;
+	}
+
+	/**
+	 * Construtor do menu com uma imagem e um título.
+	 * 
+	 * @param titulo
+	 * @param imagem
+	 */
 	public MenuItem(String titulo, ResourceReference imagem) {
 		this(titulo, imagem, null);
 	}
 
 	/**
+	 * @see MenuItem#getTitulo()
+	 * 
 	 * @return the titulo
 	 */
 	public String getTitulo() {
@@ -97,13 +121,17 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param titulo the titulo to set
+	 * Título ou label do item de menu.
+	 * @param titulo
+	 *            the titulo to set
 	 */
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
 
 	/**
+	 * @see MenuItem#setWidth(Integer)
+	 * 
 	 * @return the width
 	 */
 	public Integer getWidth() {
@@ -111,13 +139,18 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param width the width to set
+	 * Largura do item de menu. Caso não especificada será considerada a largura
+	 * definida nas configurações de níveis.
+	 * 
+	 * @param width
+	 *            the width to set
 	 */
 	public void setWidth(Integer width) {
 		this.width = width;
 	}
 
 	/**
+	 * @see MenuItem#setHeight(Integer)
 	 * @return the height
 	 */
 	public Integer getHeight() {
@@ -125,13 +158,18 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param height the height to set
+	 * Altura do item de menu. Caso não especificado será considerado a altura
+	 * definida nas configurações de níveis.
+	 * 
+	 * @param height
+	 *            the height to set
 	 */
 	public void setHeight(Integer height) {
 		this.height = height;
 	}
 
 	/**
+	 * @see MenuItem#setPage(String)
 	 * @return the page
 	 */
 	public String getPage() {
@@ -139,13 +177,17 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param page the page to set
+	 * Atribui a página que será chamada quando o item de menu for clicado.
+	 * 
+	 * @param page
+	 *            the page to set
 	 */
 	public void setPage(String page) {
 		this.page = page;
 	}
 
 	/**
+	 * @see MenuItem#setToolTip(String)
 	 * @return the toolTip
 	 */
 	public String getToolTip() {
@@ -153,13 +195,18 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param toolTip the toolTip to set
+	 * Dica a ser mostrada quando o mouse estiver por cima deste item de menu.
+	 * 
+	 * @param toolTip
+	 *            the toolTip to set
 	 */
 	public void setToolTip(String toolTip) {
 		this.toolTip = toolTip;
 	}
 
 	/**
+	 * Alvo da janela que se abrirá quando o item de menu for clicado.
+	 * 
 	 * @return the target
 	 */
 	public String getTarget() {
@@ -167,7 +214,8 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param target the target to set
+	 * @param target
+	 *            the target to set
 	 */
 	public void setTarget(String target) {
 		this.target = target;
@@ -181,25 +229,31 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param align the align to set
+	 * Alinhamento do texto do label (título) do menu.
+	 * 
+	 * @param align
+	 *            the align to set
 	 */
 	public void setAlign(String align) {
 		this.align = align;
 	}
 
 	/**
+	 * Lista de sub-menus adicionados.
+	 * 
 	 * @return the subMenus
 	 */
 	List<MenuItem> getSubMenus() {
 		return subMenus;
 	}
-	
+
 	/**
-	 * Adicionar um submenu.
+	 * Adicionar um submenu neste item de menu.
+	 * 
 	 * @param menuItem
-	 * @return 
+	 * @return
 	 */
-	public MenuItem addSubMenu(MenuItem ... menuItem){
+	public MenuItem addSubMenu(MenuItem... menuItem) {
 		this.subMenus.addAll(Arrays.asList(menuItem));
 		return this;
 	}
@@ -212,7 +266,10 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param imagem the imagem to set
+	 * Referência ao recurso de imagem do botão do item de menu.
+	 * 
+	 * @param imagem
+	 *            the imagem to set
 	 */
 	void setImagem(ResourceReference imagem) {
 		this.imagem = imagem;
@@ -226,13 +283,17 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param onclick the onclick to set
+	 * Função javascript a ser chamada quando o botão for clicado.
+	 * 
+	 * @param onclick
+	 *            the onclick to set
 	 */
 	void setOnclick(String onclick) {
 		this.onclick = onclick;
 	}
 
 	/**
+	 * @see MenuItem#setOnmouseover(String)
 	 * @return the onmouseover
 	 */
 	String getOnmouseover() {
@@ -240,13 +301,18 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param onmouseover the onmouseover to set
+	 * Função javascript que é acionada quando o mouse está por cima do item de
+	 * menu.
+	 * 
+	 * @param onmouseover
+	 *            the onmouseover to set
 	 */
 	void setOnmouseover(String onmouseover) {
 		this.onmouseover = onmouseover;
 	}
 
 	/**
+	 * @see MenuItem#setOnmouseout(String)
 	 * @return the onmouseout
 	 */
 	String getOnmouseout() {
@@ -254,10 +320,13 @@ public class MenuItem implements Serializable{
 	}
 
 	/**
-	 * @param onmouseout the onmouseout to set
+	 * Função javascript quando o mouse sai de cima do item de menu.
+	 * 
+	 * @param onmouseout
+	 *            the onmouseout to set
 	 */
 	void setOnmouseout(String onmouseout) {
 		this.onmouseout = onmouseout;
 	}
-	
+
 }
