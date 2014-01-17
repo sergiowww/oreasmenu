699efdfe9602c753b839d78794d0bba62fb3ea00
sergiowww@gmail.com <sergiowww@gmail.com@dec59da4-91f7-11de-9897-6f94e350a523>
2014-01-17 20:22:41 +0000

    Inclusão de novas funcionalidades

--- tests/test-menu2/montagem-menu.js
+++ tests/test-menu2/montagem-menu.js
@@ -23,6 +23,15 @@ MenuBar.menuBar6 = function() {
 		
 		
 		var menu1_1 = new MenuItem("Cadastro de Modelo de Aviso/Edital", null, null, null, null, null, null, null, null, null).setParentMenuItem(menu1);
+		{
+			var menu1_1_0 = new MenuItem("Manter Modelo de Aviso/Edital", null, null, null, null, null, null, null, null, null).setParentMenuItem(menu1_1);
+			{
+				var menu1_1_0_0 = new MenuItem("Incluir", null, null, null, null, null, null, null, null, null).setParentMenuItem(menu1_1_0);
+				var menu1_1_0_1 = new MenuItem("Consultar", null, null, null, null, null, null, null, null, null).setParentMenuItem(menu1_1_0);
+				
+			}
+			
+		}
 		var menu1_2 = new MenuItem("Documentos/Infrações", null, null, null, null, null, null, null, null, null).setParentMenuItem(menu1);
 		var menu1_3 = new MenuItem("Configuração", null, null, null, null, null, null, null, null, null).setParentMenuItem(menu1);
 		var menu1_4 = new MenuItem("Manter Comunicado", null, null, null, null, null, null, null, null, null).setParentMenuItem(menu1);
@@ -57,8 +66,8 @@ MenuBar.menuBar6 = function() {
 	menus.push(menu2);
 	menus.push(menu3);
 	var factorymenuBar = new FactoryMenu(menus, "menuPrincipal", "menuPrincipal", "top");
-	factorymenuBar.addNivel(new Nivel(Nivel.ORIENTACAO_HORIZONTAL, "menuNivel1", "menuNivel1-Hover", null, 120, 22, Nivel.TAMANHO_EM_PX, false, Nivel.NAO_EXPANDIR_SUBNIVEL));
-	factorymenuBar.addNivel(new Nivel(Nivel.ORIENTACAO_HORIZONTAL, "menuNivel2", "menuNivel2-Hover", null, null, null, Nivel.TAMANHO_EM_PX, false, Nivel.NAO_EXPANDIR_SUBNIVEL));
+	factorymenuBar.addNivel(new Nivel(Nivel.ORIENTACAO_HORIZONTAL, "menuNivel1", "menuNivel1-Hover", null, 120, 22, Nivel.TAMANHO_EM_PX, false, Nivel.NAO_EXPANDIR_SUBNIVEL).setAjusteDistanciaMenu(-5));
+	factorymenuBar.addNivel(new Nivel(Nivel.ORIENTACAO_HORIZONTAL, "menuNivel2", "menuNivel2-Hover", null, null, null, Nivel.TAMANHO_EM_PX, false, Nivel.NAO_EXPANDIR_SUBNIVEL).setAjusteDistanciaMenu(0));
 	factorymenuBar.addNivel(new Nivel(Nivel.ORIENTACAO_HORIZONTAL, "menuNivel3", "menuNivel3", null, null, null, Nivel.TAMANHO_EM_PX, true, Nivel.EXPANDIR_SUBNIVEL));
 	factorymenuBar.addNivel(new Nivel(Nivel.ORIENTACAO_VERTICAL, "menuNivel4", "menuNivel4", null, null, null, Nivel.TAMANHO_EM_PX, true, Nivel.NAO_EXPANDIR_SUBNIVEL));
 	factorymenuBar.construirMenu();
